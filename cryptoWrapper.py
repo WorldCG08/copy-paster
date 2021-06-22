aff13d4b3df3084884287c2d5e2b34727ead0cd7
ymelnyk <yurii.melnyk@dpgmm.net>
2021-06-22 13:04:47 +0300

    configured database connection and returning list of clips to list

---
+++ cryptoWrapper.py
@@ -0,0 +1,11 @@
+import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import uuid
+
+
+# class CryptoWrapper:
+#
+#     def __init__(self):
+#
--- main.py
+++ main.py
@@ -1,23 +1,43 @@
+import os
+import sqlite3
+
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QApplication
 import sys
+import time
+import pyclip
 
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Ui, self).__init__() # Call the inherited classes __init__ method
-        uic.loadUi('pyclip.ui', self) # Load the .ui file
+        super(Ui, self).__init__()
+        uic.loadUi('pyclip.ui', self)
 
-        self.sh_btn = self.findChild(QtWidgets.QPushButton, 'show_hide_btn')
-        self.sh_btn.setText('Text Changed2')
-        self.sh_btn.clicked.connect(self.sh_btn_pressed)
-        self.line_text = self.findChild(QtWidgets.QLineEdit, 'line_text')
-        self.line_text.setText('Oppapapa')
-        self.show() # Show the GUI
+        self.hidden = True  #Hide clips at the start
+
+        if os.path.exists('.' + os.sep + 'copypaste.db'):
+            self.connection = sqlite3.connect('copypaste.db')
+        else:
+            self.connection = sqlite3.connect('copypaste.db')
+            self.connection.cursor().execute('''CREATE TABLE clips (id INTEGER	constraint clips_pk primary key autoincrement,
+             clip text, created text)''')
+
+        self.connection.row_factory = lambda cursor, row: row[0]
+        self.cliplist = self.findChild(QtWidgets.QListWidget, 'clip_list')
+        self.cliplist.addItems(self.connection.cursor().execute("select clip from clips").fetchall())
+
+
+        # self.sh_btn = self.findChild(QtWidgets.QPushButton, 'show_hide_btn')
+        # self.sh_btn.setText('Text Changed2')
+        # self.sh_btn.clicked.connect(self.sh_btn_pressed)
+        # self.line_text = self.findChild(QtWidgets.QLineEdit, 'line_text')
+        # self.line_text.setText('Oppapapa')
+        self.show()
 
     def sh_btn_pressed(self):
         self.line_text.setText('miyagi')
         print(self.line_text.text())
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
