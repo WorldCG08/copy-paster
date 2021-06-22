2526f02e45562fa8793282e07c2dc1892da5064d
ymelnyk <yurii.melnyk@dpgmm.net>
2021-06-22 14:12:18 +0300

    Saving clips to db

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import sqlite3
-
-from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QApplication
+import threading
 import sys
+from datetime import date
 import time
 import pyclip
+from PyQt5 import QtWidgets, uic
 
 
 class Ui(QtWidgets.QMainWindow):
@@ -23,10 +23,14 @@ class Ui(QtWidgets.QMainWindow):
              clip text, created text)''')
 
         self.connection.row_factory = lambda cursor, row: row[0]
-        self.cliplist = self.findChild(QtWidgets.QListWidget, 'clip_list')
-        self.cliplist.addItems(self.connection.cursor().execute("select clip from clips").fetchall())
+        self.cursor = self.connection.cursor()
 
+        self.cliplist = self.cursor.execute("select clip from clips").fetchall()
+        self.cliplist_widget = self.findChild(QtWidgets.QListWidget, 'clip_list')
+        self.cliplist_widget.addItems(self.cliplist)
 
+        x = threading.Thread(target=self.clip_listener)
+        x.start()
         # self.sh_btn = self.findChild(QtWidgets.QPushButton, 'show_hide_btn')
         # self.sh_btn.setText('Text Changed2')
         # self.sh_btn.clicked.connect(self.sh_btn_pressed)
@@ -37,6 +41,19 @@ class Ui(QtWidgets.QMainWindow):
     def sh_btn_pressed(self):
         self.line_text.setText('miyagi')
         print(self.line_text.text())
+
+    def clip_listener(self):
+        connection = sqlite3.connect('copypaste.db')
+        cursor = connection.cursor()
+        while True:
+            time.sleep(1)
+            clip = pyclip.paste().decode().strip("'").replace("'", "''")  # text will have the content of clipboard
+            if clip not in self.cliplist:
+                td = date.today()
+                cursor.execute(f"INSERT INTO clips (clip, created) VALUES ('{clip}','{td}')")
+                connection.commit()
+                self.cliplist.append(clip)
+                self.cliplist_widget.insertItem(0, clip)
 
 
 if __name__ == '__main__':
